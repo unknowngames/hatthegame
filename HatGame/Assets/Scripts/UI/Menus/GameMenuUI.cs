b8f64f651e602e9ba2a8b5703e149f2d3f1d0511
katsukosensey <dilyarkashamsutdinova@gmail.com>
2015-07-04 23:02:19 +0400

    структура игрового меню

--- HatGame/Assets/Scripts/UI/Menus/GameMenuUI.cs
+++ HatGame/Assets/Scripts/UI/Menus/GameMenuUI.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Assets._scripts.UI.Base;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts.HatGameUnity;
 
 namespace Assets.Scripts.UI.Menus
 {
     class GameMenuUI : UIWindow
     {
+		Team currentTeam;
+		[SerializeField]
+		Text player1;
+		[SerializeField]
+		Text player2;
+		[SerializeField]
+		Image icon;
+		[SerializeField]
+		Text scores;
+		[SerializeField]
+		GameView gameView;
+
+		public void SetCurrentTeam(Team team)
+		{
+			currentTeam = team;
+		}
+
+		void OnEnable()
+		{
+			base.OnEnable ();
+			player1.text = currentTeam.PlayerOne;
+			player2.text = currentTeam.PlayerTwo;
+			icon.sprite = currentTeam.TeamIcon.sprite;
+		}
+
+		void Update()
+		{
+			if (currentTeam != null && scores != null && gameView != null) 
+			{
+				scores.text = (currentTeam.Scores + gameView.Points).ToString();
+			}
+		}
+
     }
 }
